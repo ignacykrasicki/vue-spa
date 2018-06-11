0cd0871c85334c98af20c5af7677348bef601158
ignacykrasicki <ignacykrasicki@wp.pl>
2018-06-11 22:14:31 +0200

    q

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,6 +1,8 @@
 
 const webpack = require("webpack");
 const clientConfig = require("./webpack.client.config");
+const serverConfig = require("./webpack.server.config");
+const MFS = require('memory-fs')
 
 module.exports = function setupDevServer(app) {
   clientConfig.entry.app = [
@@ -21,4 +23,12 @@ module.exports = function setupDevServer(app) {
   );
 
   app.use(require("webpack-hot-middleware")(clientCompiler));
+
+  const serverCompiler = webpack(serverConfig)
+  const mfs = new MFS()
+  const outputPath = path.join(serverConfig.output.path, '/server/main.js')
+  serverCompiler.outputFileSystem = mfs
+  serverCompiler.watch({}, () => {
+    onUpdate(mfs, readFileSync(outputPath, 'utf-8'))
+  })
 };
